c58541610e65e4b53a8f727566e26e7f0aaf06bf
Anthony Khong <anthony.kusumo.khong@gmail.com>
2021-10-29 18:45:46 +0700

    use opaque types to validate coordinate and cell (#10)

--- packages/core/src/lib/core.ts
+++ packages/core/src/lib/core.ts
@@ -1,9 +1,38 @@
-import { Value, Cell } from './types';
+import * as t from './types';
 
-export function toCell(value: Value): Cell {
+const MAX_ROWS = Math.round(1e5);
+const MAX_COLS = Math.round(1e4);
+
+export function toCell(value: t.Value): t.Cell {
   return {
     value: value,
     coord: { row: 0, col: 0 },
     style: {},
   };
+}
+
+export function validateCoord(coord: t.Coord): t.ValidCoord | t.Error {
+  if (
+    coord.row >= 0 &&
+    coord.row <= MAX_ROWS &&
+    coord.col >= 0 &&
+    coord.col <= MAX_COLS
+  ) {
+    return coord as t.ValidCoord;
+  } else {
+    return { error: 'invalid coordinate range' };
+  }
+}
+
+export function validateCell(cell: t.Cell): t.ValidCell | t.Error {
+  const messages = [];
+  const coordValidation = validateCoord(cell.coord);
+  if (t.isError(coordValidation)) {
+    messages.push(coordValidation);
+  }
+  if (messages.length == 0) {
+    return cell as t.ValidCell;
+  } else {
+    return { error: messages.join('\n') };
+  }
 }
