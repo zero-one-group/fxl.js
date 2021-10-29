3e14e14641ca9ca816cb39a51340768f3fd930ad
anthony-khong <anthony.kusumo.khong@gmail.com>
2021-10-29 18:43:56 +0700

    use opaque types to validate coordinate and cell

--- packages/core/src/lib/core.spec.ts
+++ packages/core/src/lib/core.spec.ts
@@ -1,5 +1,24 @@
 import * as fxl from './core';
 
+describe('validators', () => {
+  const validCoord = { row: 0, col: 0 };
+  const validCell = { value: 'abc', coord: validCoord };
+  const invalidCoord = { row: -1, col: 0 };
+  const invalidCell = { value: 'abc', coord: invalidCoord };
+
+  it('validateCoord should work', () => {
+    expect(fxl.validateCoord(validCoord)).toEqual(validCoord);
+    const validCoordWithSheet = { ...validCoord, sheet: 'ABC' };
+    expect(fxl.validateCoord(validCoordWithSheet)).toEqual(validCoordWithSheet);
+    expect(fxl.validateCoord(invalidCoord)).toHaveProperty('error');
+  });
+
+  it('validateCell should work', () => {
+    expect(fxl.validateCell(validCell)).toEqual(validCell);
+    expect(fxl.validateCell(invalidCell)).toHaveProperty('error');
+  });
+});
+
 describe('core', () => {
   it('toCell should have sensible defaults', () => {
     const cellValue = 'abc-xyz';
